a41f976069cd450988690c3ef110a3d55e584a72
unknown <caro200288@gmail.com>
2026-09-08 23:36:45 -0400

    chore: align production asset version

--- version.js
+++ version.js
@@ -1,4 +1,4 @@
 export const APP_VERSION='0.7.7';
 export const APP_LABEL='V0.7.7 PRODUCTION';
-export const ASSET_VERSION='0772';
+export const ASSET_VERSION='0773';
 export const BACKUP_SCHEMA_VERSION=3;
